1ed7472cedcdd0ae4c0cd60e8ddad4ce4a60d420
CEOXeon <127042438+CEOXeon@users.noreply.github.com>
2023-06-07 20:06:52 +0200

    1h wasted for old Compose Tutorial

--- app/src/main/java/com/ceoxeon/android/template/app/MainActivity.kt
+++ app/src/main/java/com/ceoxeon/android/template/app/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ceoxeon.android.template.app.ui.theme.CEOXeonAndroidTemplateAppTheme
+import com.ceoxeon.android.template.app.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +23,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Android and CEOXeon")
+                    CustomText(text = "I wasted 1h for this")
                 }
             }
         }
@@ -36,6 +38,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+
+@Composable
+fun CustomText(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "$text",
+        modifier = modifier
+        /* style = Typography.h5; I have watched an old tutorial where Compose was in Beta so this doesn't work */
+    )
+}
+
 
 @Preview(showBackground = true)
 @Composable
